db6f7a58441e3dae92d844445d918c4f4d5a86f8
thehienliu <liuthehien666@gmail.com>
2024-07-12 14:40:34 +0000

    Add configuration and run file.

---
+++ run.py
@@ -0,0 +1,117 @@
+import os
+import cv2
+import torch
+import argparse
+import albumentations as A
+from omegaconf import OmegaConf
+from torch.utils.data import DataLoader
+from cell_segmentation.models.cellmamba import CellMamba
+from cell_segmentation.utils.get_loss import get_loss_fn
+from cell_segmentation.datasets.pannuke import CustomCellSeg
+from cell_segmentation.trainer.trainer_cellmamba import CellMambaTrainer
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train CellMamba model for cell segmentation task.")
+    parser.add_argument("--config", type=str, help="Configuration yaml file path.")
+    parser.add_argument("--output", type=str, default="output", help="Configuration yaml file path.")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+  # Get config
+  args = parse_args()
+  config = OmegaConf.load(args.config)
+
+  # Setup Class
+  tissue_types = {}
+
+  for filename in os.listdir('/content/fold1/images'):
+      tissue_type = filename.split('type_')[-1].split('.')[0]
+      if tissue_type not in tissue_types:
+          tissue_types[tissue_type] = len(tissue_types)
+
+  # Setup transform
+  trans = config.transformations
+  input_shape = 256
+
+  train_transform = A.Compose([
+            A.RandomRotate90(p=trans.randomrotate90.p),
+            A.HorizontalFlip(p=trans.horizontalflip.p),
+            A.VerticalFlip(p=trans.verticalflip.p),
+            A.Downscale(p=trans.downscale.p, scale_max=trans.downscale.scale, scale_min=trans.downscale.scale, interpolation=cv2.INTER_AREA),
+            A.Blur(p=trans.blur.p, blur_limit=trans.blur.blur_limit),
+            A.GaussNoise(p=trans.gaussnoise.p, var_limit=trans.gaussnoise.var_limit),
+            A.ColorJitter(
+                            p=trans.colorjitter.p,
+                            brightness=trans.colorjitter.scale_setting,
+                            contrast=trans.colorjitter.scale_setting,
+                            saturation=trans.colorjitter.scale_color,
+                            hue=trans.colorjitter.scale_color / 2,
+                        ),
+            A.Superpixels(
+                            p=trans.superpixels.p,
+                            p_replace=0.1,
+                            n_segments=200,
+                            max_size=int(input_shape / 2),
+                        ),
+            A.ZoomBlur(p=trans.zoomblur.p, max_factor=1.05),
+            A.RandomSizedCrop(
+                            min_max_height=(input_shape / 2, input_shape),
+                            height=input_shape,
+                            width=input_shape,
+                            p=trans.randomsizedcrop.p,
+                        ),
+            A.ElasticTransform(p=trans.elastictransform.p, sigma=25, alpha=0.5, alpha_affine=15),
+            A.Normalize(mean=trans.normalize.mean, std=trans.normalize.std)
+      ])
+
+  valid_transform = A.Compose([A.Normalize(mean=trans.normalize.mean, std=trans.normalize.std)])
+  test_transform = A.Compose([A.Normalize(mean=trans.normalize.mean, std=trans.normalize.std)])
+
+
+  # Setup dataset
+  train_data = CustomCellSeg(image_dir=config.data.train.image_dir,
+                            label_dir=config.data.train.label_dir,
+                            class_names=tissue_types,
+                            transforms=train_transform)
+
+  valid_data = CustomCellSeg(image_dir=config.data.val.image_dir,
+                            label_dir=config.data.val.label_dir,
+                            class_names=tissue_types,
+                            transforms=valid_transform)
+
+  test_data = CustomCellSeg(image_dir=config.data.test.image_dir,
+                            label_dir=config.data.test.label_dir,
+                            class_names=tissue_types,
+                            transforms=test_transform)
+
+  # Setup training
+  device = 'cuda' if torch.cuda.is_available() else 'cpu'
+  model = CellMamba(num_classes=len(tissue_types)).to(device)
+  loss_fn_dict = get_loss_fn()
+  optimizer = torch.optim.AdamW(model.parameters(), betas=(0.85, 0.95), weight_decay=0.0001)
+  scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.85)
+
+  # Setup data loader
+  train_dataloader = DataLoader(train_data, batch_size=config.training.batch_size, shuffle=True)
+  valid_dataloader = DataLoader(valid_data, batch_size=config.training.batch_size, shuffle=False)
+  test_dataloader = DataLoader(test_data, batch_size=config.training.batch_size, shuffle=False)
+
+  # Setup trainer
+  trainer = CellMambaTrainer(model=model,
+                           loss_fn_dict=loss_fn_dict,
+                           optimizer=optimizer,
+                           scheduler=scheduler,
+                           device=device,
+                           num_classes=6, # This is nuclei class sr please fix the name for me
+                           logdir=args.output, # a
+                           early_stopping=None)
+
+  # Fit
+  trainer.fit(epochs=config.training.epochs,
+            train_dataloader=train_dataloader,
+            val_dataloader=valid_dataloader,
+            metric_init=None,
+            eval_every=5)
